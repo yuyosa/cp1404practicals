6abcadc1d81bf06ef221866928d163e354b6b2dc
Haojia Li <haojia.li@my.jcu.edu.au>
2024-07-21 15:05:02 +0800

    prac_07
    @Dreaki-1,This is my prac_07. If possible, please help me check if there are any errors in the details. Thank you.

---
+++ prac_07/guitar.py
@@ -0,0 +1,24 @@
+class Guitar:
+    def __init__(self, name = "", year = 0, cost = 0):
+        self.name = name
+        self.year = int(year)
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name}  ({self.year} :{self.cost})"
+
+
+
+    def get_age(self):
+        current_year = 2022
+        return current_year - self.year
+
+
+
+    def is_vintage(self):
+        return self.get_age() >= 50
+
+    def __lt__(self, other):
+        return self.year < other.year
+
+# Complete first part of More Guitars exercise
---
+++ prac_07/project.py
@@ -0,0 +1,15 @@
+class Project:
+    def __init__(self,name, start_date, priority, cost_estimate, completion_percentage):
+        self.name = name
+        self.start = start_date
+        self.priority = priority
+        self.cost = cost_estimate
+        self.completion = completion_percentage
+
+
+
+
+    def __repr__(self):
+        start_date_str = self.start.strftime("%d/%m/%Y")
+        return (f"{self.name}, start: {start_date_str}, priority {self.priority}, "
+                f"estimate: ${self.cost:.2f}, completion: {self.completion}%")
